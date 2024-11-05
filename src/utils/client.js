e6a446b1648d327c22f9d77fb7a3fe0dffc672ba
fmontes <751424+fmontes@users.noreply.github.com>
2024-11-05 14:24:46 -0600

    feat(client): Add no-cache to requestOptions

--- src/utils/client.js
+++ src/utils/client.js
@@ -4,4 +4,8 @@ import { DotCmsClient } from "@dotcms/client";
 export const client = DotCmsClient.init({
     dotcmsUrl: process.env.NEXT_PUBLIC_DOTCMS_HOST,
     authToken: process.env.NEXT_PUBLIC_DOTCMS_AUTH_TOKEN,
+    requestOptions: {
+        // In production you might want to deal with this differently
+        cache: "no-cache",
+    }
 });
