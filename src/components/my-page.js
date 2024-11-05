1a61cb55352d56c119bb3502881019fc92157143
fmontes <751424+fmontes@users.noreply.github.com>
2024-11-05 14:59:33 -0600

    chore: render banner component.

--- src/components/my-page.js
+++ src/components/my-page.js
@@ -4,15 +4,17 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import { DotcmsLayout } from "@dotcms/react";
 import { usePageAsset } from "@/hooks/usePageAsset";
+import Banner from './Banner';
 
-const componentsMap = new Proxy(
-    {},
-    {
-        get: (target, prop) =>
-            target[prop] ||
-            ((contentlet) => <div>{contentlet.contentType}</div>),
-    }
-);
+const components = {
+  Banner: (contentlet) => <Banner {...contentlet} />,
+};
+
+const componentsMap = new Proxy(components, {
+  get: (target, prop) =>
+    target[prop] ||
+    ((contentlet) => <div>{contentlet.contentType}</div>),
+});
 
 export function MyPage({ pageAsset }) {
     const pathname = usePathname();
