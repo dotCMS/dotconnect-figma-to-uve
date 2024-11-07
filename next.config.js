22dc927a5057ade817fa126793236b2770e60c19
fmontes <751424+fmontes@users.noreply.github.com>
2024-11-07 08:48:02 -0600

    clean up 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const url = new URL(process.env.NEXT_PUBLIC_DOTCMS_HOST);
+
 const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8080',
+                protocol: url.protocol.slice(0, -1),
+                hostname: url.hostname,
+                port: url.port,
                 pathname: '/dA/**',
             },
         ],
